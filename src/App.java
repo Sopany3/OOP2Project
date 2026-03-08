d9ccc75a2881c46b451eb34b0c0c34ef7b542451
Mondragon <mondragonjacerrein@gmail.com>
2026-03-08 20:48:25 +0800

    added some basic code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.*;
 
-        System.out.println("Wassup dawg");
+public class App{
+    public static void main(String[]args){
 
-        System.out.println("Niggalataions");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Damn dawg, welcome to Rock Paper Scissors.");
+
+        for (int i = 10; i <= 100; i+=10){
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.print(" " + i + "%");
+        }
+
+        System.out.println("\nGame Ready!");
+
+        System.out.println("\n-----------System------------");
+        System.out.print("Choose your Character:\n[0]Seth\n[1]Seraph\n[2]Aurelius\n[3]Agnis\n[4]Verlierer\n[5]Fvyina\nChoose: ");
+        int chooseCharacter = scanner.nextInt();
+        scanner.nextLine();
+
+
     }
 }
